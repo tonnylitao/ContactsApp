6b3c6c6d80028e790b00a927621a5044a3d6d1e7
Tonny <tonny.litao@gmail.com>
2020-05-28 09:45:37 +1200

    Update OptionalEx.swift

--- iOS/Contacts/Extension/OptionalEx.swift
+++ iOS/Contacts/Extension/OptionalEx.swift
@@ -11,26 +11,37 @@ import Foundation
 extension Optional {
     
     /*
-     
-     if let a = optional {
-     }else {
-     }
-     
-     switch optional {
-     case .some(let a) {
-     }
-     default: break
-     }
-     
-     --------------------------
-     
-     optional.ifSome {
-        $0
-     }
-     
-     */
+    1
+    if let a = optional {
+    }else {
+    }
+    
+    2
+    switch optional {
+    case .some(let a) {
+    }
+    default: break
+    }
+    
+    3
+    optional.map {
+       
+    }
+    
+    4
+    optional.flatMap {
+       
+    }
+    
+    --------------------------
+    5
+    optional.ifSome {
+       $0
+    }
+    
+    */
     
     func ifSome(_ transform: (Wrapped) throws -> Void) rethrows {
-        try flatMap(transform)
+        try map(transform)
     }
 }
