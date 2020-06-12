8ef62c14ff3ab0d3e8fa87c2d8880256f76e7bba
Tonny <tonny.litao@gmail.com>
2020-06-13 06:01:34 +1200

    update search view

--- Android/app/src/main/java/com/tonnysunm/contacts/ui/search/SearchActivity.kt
+++ Android/app/src/main/java/com/tonnysunm/contacts/ui/search/SearchActivity.kt
@@ -1,10 +1,13 @@
 package com.tonnysunm.contacts.ui.search
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.tonnysunm.contacts.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
@@ -12,10 +15,14 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        val imm =
+            searchView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
-        
+
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
     }
