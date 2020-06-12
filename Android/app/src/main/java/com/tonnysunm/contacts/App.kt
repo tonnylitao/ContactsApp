3e36fb8ab209b589c8c6612a28108c989c0e53eb
Tonny <tonny.litao@gmail.com>
2020-06-12 13:49:15 +1200

    add navigation

---
+++ Android/app/src/main/java/com/tonnysunm/contacts/App.kt
@@ -0,0 +1,15 @@
+package com.tonnysunm.contacts
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
--- Android/app/src/main/java/com/tonnysunm/contacts/MainActivity.kt
+++ Android/app/src/main/java/com/tonnysunm/contacts/MainActivity.kt
@@ -1,23 +1,5 @@
 package com.tonnysunm.contacts
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.tonnysunm.contacts.ui.main.MainFragment
-import timber.log.Timber
 
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
