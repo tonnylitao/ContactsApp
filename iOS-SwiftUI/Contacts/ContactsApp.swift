49680689c446fa8ac5c89e70df3985427319c4c1
Tonny <tonny.litao@gmail.com>
2021-03-17 19:56:50 +1300

    seperate iOS as uikit and swiftui

---
+++ iOS-SwiftUI/Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Tonny on 1/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
